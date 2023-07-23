5fde83bc2ad7151dc844e9965db1764dd8136321
Julia Amaral <105997355+juliaamarall@users.noreply.github.com>
2023-07-23 17:23:41 -0300

    commit da 1º pasta

---
+++ calculadora de idade/script.js
@@ -0,0 +1,16 @@
+var fm = document.getElementsByTagName('form')[0];
+var menssagem = document.getElementById('old');
+fm.addEventListener("submit", function(event){
+    event.preventDefault();
+
+
+    var anoNas = document.getElementById('anox').value;
+    const dataAtual = new Date();
+    const anoAtual = dataAtual. getFullYear();
+
+    var idade = (anoAtual - anoNas);
+
+
+    menssagem.textContent = idade + ' Anos';
+
+});
