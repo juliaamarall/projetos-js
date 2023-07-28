642015ce576776704213b0bde372d2e9b8974be9
Julia Amaral <105997355+juliaamarall@users.noreply.github.com>
2023-07-28 15:48:56 -0300

    commit2

--- personal info/script.js
+++ personal info/script.js
@@ -1,8 +1,26 @@
-var caixasBlocos = document.querySelectorAll('infos-left0') [0] ;
- setInterval(function(){
-    pulsar()
- }, 1800);
- 
-caixasBlocos.addEventListener("onMouseover", function pulsar(){
-   caixasBlocos.style.marginTop = '40px'
+var caixasBlocosList = document.querySelectorAll('[class*="infos-left"]');
+
+function pulsar(element) {
+   element.style.position = 'relative';
+   element.style.bottom = '2%';
+}
+
+function resetPosition(element) {
+   element.style.position = 'relative';
+   element.style.bottom = '0%';
+}
+
+
+
+caixasBlocosList.forEach(function (caixasBlocos) {
+    caixasBlocos.addEventListener("mouseover", function () {
+        pulsar(caixasBlocos);
+    });
+
+    caixasBlocos.addEventListener("mouseout", function () {
+        resetPosition(caixasBlocos);
+    });
 });
+
+
+
